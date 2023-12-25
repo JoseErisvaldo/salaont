cbddaa34f3dcc06723f52ec576657a2bd4a4afd3
Jose Erisvaldo <joseerisvaldopg@gmail.com>
2023-12-25 17:14:22 -0300

    excluir

--- src/Page/Historico/index.js
+++ src/Page/Historico/index.js
@@ -7,24 +7,32 @@ function Historico() {
     const verLocal = JSON.parse(local) || []
     setRes(verLocal)
   }, [])
+
+  function excluir(id) {
+    const filtro = resList.filter(item => {
+      return item.nomeCliente !== id
+    })
+    setRes(filtro)
+    const newLocal = localStorage.setItem('@listCorte', JSON.stringify(filtro))
+  }
+
   return (
     <div>
       <h1>Historico</h1>
       <div>
-        <div>
-          Total de cortes: {resList.length}
-        </div>
-        
+        <div>Total de cortes: {resList.length}</div>
       </div>
 
       {resList.map(item => (
-        <div className="cards">
+        <div className="cards" key={item.id}>
           <div className="cards-dados">
             <div>Nome: {item.data}</div>
             <div>Nome: {item.nomeCliente}</div>
             <div>Valor: {item.valorCorte}</div>
           </div>
-          <div className="excluir">Excluir</div>
+          <div className="excluir" onClick={e => excluir(item.nomeCliente)}>
+            Excluir
+          </div>
         </div>
       ))}
     </div>
